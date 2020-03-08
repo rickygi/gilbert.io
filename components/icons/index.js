5ef8dcb1f62295f7d55501e1b949c6152c994652
Ricky Gilbert <ricky@gilbert.io>
2020-03-08 12:18:02 -0700

    update project

---
+++ components/icons/index.js
@@ -0,0 +1,3 @@
+export { default as GitHubLogo } from './logo-github.svg';
+export { default as InstagramLogo } from './logo-instagram.svg';
+export { default as LinkedInLogo } from './logo-linkedin.svg';
