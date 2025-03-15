bbebc49bd2dc198b3520cad2ef153346ceb16541
Ricky Gilbert <ricky@gilbert-io.local>
2025-03-15 15:01:10 -0700

    update to next.js version 15

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,24 @@
 export default function Home() {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-    <div className="text-center">
-      <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">gilbert.io</h1>
-      <p className="mt-6 text-base leading-7 text-gray-600">ricky@gilbert.io</p>
-      <div className="mt-10 flex items-center justify-center gap-x-6">
-        <a href="https://twitter.com/rickygilbe" className="text-sm font-semibold text-gray-900">
-          x
-        </a>
-        <a href="https://www.instagram.com/rickygi/" className="text-sm font-semibold text-gray-900">
-          instagram
-        </a>
-        <a href="https://github.com/rickygi" className="text-sm font-semibold text-gray-900">
-          github
-        </a>
-        <a href="https://www.linkedin.com/in/rickygi/" className="text-sm font-semibold text-gray-900">
-          linkedin
-        </a>
-      </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <h1 className="text-5xl font-bold">gilbert.io</h1>
+        <p>ricky@gilbert.io</p>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a href="https://twitter.com/rickygilbe" className="flex items-center gap-2 hover:underline hover:underline-offset-4">
+            x
+          </a>
+          <a href="https://www.instagram.com/rickygi/" className="flex items-center gap-2 hover:underline hover:underline-offset-4">
+            instagram
+          </a>
+          <a href="https://github.com/rickygi" className="flex items-center gap-2 hover:underline hover:underline-offset-4">
+            github
+          </a>
+          <a href="https://www.linkedin.com/in/rickygi/" className="flex items-center gap-2 hover:underline hover:underline-offset-4">
+            linkedin
+          </a>
+        </div>
+      </main>
     </div>
-  </main>
   )
 }
